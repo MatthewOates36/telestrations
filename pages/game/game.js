d493d508c19d6421cb6fea147f3fde10ed7db08b
Matthew Oates <uni.oates@gmail.com>
2020-04-11 17:21:32 -0600

    Image and text transfer works (#1)

--- pages/game/game.js
+++ pages/game/game.js
@@ -1,7 +1,21 @@
+const socket = io('/game')
 const canvas = $("#canvas")
 const scribbleArea = new ScribbleArea(canvas)
 
 let drawMode = DrawMode.DRAW
+let currentPage = -1;
+
+let drawingPage = document.getElementById("drawingPage")
+let wordGuessingPage = document.getElementById("wordGuessingPage")
+let guessedWordInputBox = document.getElementById("wordGuessInput")
+let imageToGuessFrom = document.getElementById("imageToGuessFrom")
+let wordToBeDrawn = document.getElementById("wordToBeDrawn")
+
+hideDrawingPage()
+
+document.getElementById("doneButton").addEventListener("click", continueToNextPage)
+document.getElementById("doneButtonForGuessPage").addEventListener("click", continueToNextPage)
+
 
 canvas[0].addEventListener('touchmove', e => {
     if (e.touches.length === 1) {
@@ -36,8 +50,37 @@ $("#redoButton").on('click touchstart', (event) => {
     scribbleArea.redo()
 })
 
-setTimeout(() => {
-    let image = scribbleArea.getImage()
-    console.log(image)
-    $("#imageDisplay").css("background-image", "url(" + image + ")")
-}, 10000)
+socket.on('image', message =>{
+    let data = JSON.parse(message)
+    imageToGuessFrom.src = data.image
+    hideDrawingPage()
+    guessedWordInputBox.value = ""
+})
+
+socket.on('text', message =>{
+    let data = JSON.parse(message)
+    wordToBeDrawn.innerText = "Draw this: " + data.text
+    hideWordGuessingPage()
+    scribbleArea.clear()
+})
+
+function continueToNextPage(){
+    currentPage = currentPage += 1
+    if(currentPage % 2 === 0){
+        socket.emit('text', JSON.stringify({text: guessedWordInputBox.value}))
+    } else {
+        socket.emit('image', JSON.stringify({image: scribbleArea.getImage()}))
+    }
+    console.log(guesses)
+}
+
+function hideDrawingPage() {
+    drawingPage.style.display = "none"
+    wordGuessingPage.style.display = "block"
+
+}
+
+function hideWordGuessingPage() {
+    drawingPage.style.display = "block"
+    wordGuessingPage.style.display = "none"
+}
